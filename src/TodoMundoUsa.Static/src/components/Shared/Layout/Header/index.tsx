89842db7503c90d62facd9c3d5f10601fee0e23c
Vitor Martins <vitor.martins@fever.pt>
2021-07-27 22:10:38 +0100

    ADD header

--- src/TodoMundoUsa.Static/src/components/Shared/Layout/Header/index.tsx
+++ src/TodoMundoUsa.Static/src/components/Shared/Layout/Header/index.tsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Link from 'next/link'
 import styles from './styles.module.scss'
 
 const Header: React.FC = () => {
+	const [navActive, setNavActive] = useState(false)
+
+	const handleToggleNavActive = () => {
+		document.body.classList.toggle('no-scroll-hidden')
+		setNavActive(!navActive)
+	}
+
+	function handleClickOnMenuItem() {
+		document.body.classList.remove('no-scroll-hidden')
+		setNavActive(!navActive)
+	}
+
 	return (
 		<header className={styles.header}>
-			<a href="/">Eu sou o header</a>
+			<div className={styles.headerWrap}>
+				<Link href="/">
+					<a className={styles.headerBrand}>
+						<img
+							className="img-full-height"
+							src="/logo.svg"
+							alt="Todo Mundo USA"
+						/>
+					</a>
+				</Link>
+				<button
+					aria-label="Toggle Mobile Menu Button"
+					type="button"
+					className={`${styles.headerNavToggle} ${
+						navActive ? styles.active : ''
+					}`}
+					onClick={handleToggleNavActive}
+				>
+					<div className={styles.headerNavHamburger}>
+						<span>&nbsp;</span>
+						<span>&nbsp;</span>
+						<span>&nbsp;</span>
+						<strong>{!navActive ? 'Menu' : 'Fechar'}</strong>
+					</div>
+				</button>
+				<nav
+					className={`${styles.headerNav} ${
+						navActive ? styles.active : ''
+					}`}
+				>
+					<div className={styles.headerNavWrap}>
+						<ul className={styles.headerNavList}>
+							<li
+								onClick={handleClickOnMenuItem}
+								className={styles.headerNavListItem}
+							>
+								<Link href="/home">
+									<a>nav item</a>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
 		</header>
 	)
 }
